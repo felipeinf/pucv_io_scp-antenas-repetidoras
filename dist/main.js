1d4878ff758a4c13196ec2102425edc78b04e3af
felipeinf <fconcha.almeida@gmail.com>
2019-08-19 23:48:26 -0400

    fix: add dist in .gitignore

--- dist/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var solver_1 = require("./model/solver");
-var solver = new solver_1.Solver();
-solver.buscarSolucion();
